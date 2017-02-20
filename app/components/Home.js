f40d92cc59acb694855f078033f1725f8a49b823
Patrick Gidich <patrick@simnova.com>
2017-02-20 07:40:39 -0500

    LatestnUIuPdates

--- app/components/Home.js
+++ app/components/Home.js
@@ -13,10 +13,10 @@ export default class Home extends Component {
           <div className={styles.navigationContainer}>
             <div className={styles.contentWedge}></div>
             <div className={styles.navigationLink}><Link to="/WhyStart"><div><strong>Why</strong> start with a VEGFR Inhibitor? <img src="assets/images/forward-arrow.png"/></div></Link></div><br/>
-            <div className={styles.navigationLink}><Link to="/WhoIs"><div><strong>Who</strong> is the appropriate patient for Votrient? <img src="assets/images/forward-arrow.png"/></div></Link></div><br/>
+            <div className={styles.navigationLink}><Link to="/WhoIs"><div><strong>Who</strong> is an appropriate patient for Votrient? <img src="assets/images/forward-arrow.png"/></div></Link></div><br/>
             <div className={styles.navigationLink}><Link to="/WhatIs"><div><strong>What</strong> is the PFS benefit of Votrient First-Line? <img src="assets/images/forward-arrow.png"/></div></Link></div><br/>
             <div className={styles.navigationLink}><Link to="/HowDoes"><div><strong>How</strong> does votrient compare with Sunitinib First-Line? <img src="assets/images/forward-arrow.png"/></div></Link></div><br/>
-            <div className={styles.navigationLink}><Link to="/WhatIsSafety"><div><strong>What</strong> is the safety profile of Votrient? <img src="assets/images/forward-arrow.png"/></div></Link></div><br/>
+            <div className={styles.navigationLink}><Link to="/WhatIsSafety"><div><strong>What</strong> is the safety profile for Votrient? <img src="assets/images/forward-arrow.png"/></div></Link></div><br/>
             <div className={styles.navigationLink}><Link to="/Which"><div><strong>Which</strong> First-Line treatment do patients prefer? <img src="assets/images/forward-arrow.png"/></div></Link></div><br/>
             <div className={styles.navigationLink}><Link to="/Why"><div><strong>Why</strong> Votrient? <img src="assets/images/forward-arrow.png"/></div></Link></div><br/>
           </div>
